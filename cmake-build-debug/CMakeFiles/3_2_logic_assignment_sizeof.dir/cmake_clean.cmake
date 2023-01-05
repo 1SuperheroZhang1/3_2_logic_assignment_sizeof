2663a5ee65bba3f01c93f253a7f3ab672771441b
zhangcy <2304417358@qq.com>
2023-01-05 16:42:31 +0800

    C语言督学营 - 3.2 逻辑运算符、赋值运算符和求字节运算符sizeof

---
+++ cmake-build-debug/CMakeFiles/3_2_logic_assignment_sizeof.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3_2_logic_assignment_sizeof.exe"
+  "3_2_logic_assignment_sizeof.exe.manifest"
+  "3_2_logic_assignment_sizeof.pdb"
+  "CMakeFiles/3_2_logic_assignment_sizeof.dir/main.c.obj"
+  "lib3_2_logic_assignment_sizeof.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3_2_logic_assignment_sizeof.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
